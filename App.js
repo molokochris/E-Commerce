17a821e3bdf81f91b4042832692088c5d1ffff1e
khomotsojane <khomotsojane27@gmail.com>
2023-10-12 12:37:10 +0200

    KHOMOTSO

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './components/screens/Welcome';
+import Login from './components/auth/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor:'blue'
+
   },
 });
