7774ab929c74966d77bf2fd0c566d9142f2385a6
molokochris <molokochrisp742@gmail.com>
2023-10-17 08:43:48 +0200

    fix textalign

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -122,98 +122,96 @@
 // });
 // export default Login;
 
-
-
-
-
-import React from 'react'
-import { TextInput } from 'react-native'
-import { StyleSheet, Text } from 'react-native'
-import { View, TouchableOpacity } from 'react-native'
+import React from "react";
+import { TextInput } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 
 export default function Login() {
   return (
     <View style={styles.container}>
-    <View style={styles.welcome}>
-        <Text style={{marginTop:150, color:'white',fontWeight:'bold',fontSize:30}}>Hello,</Text>
-        <Text style={{color:'white',fontWeight:'bold',fontSize:30}}>Wecome back!</Text>
-    </View>
-    <View style={styles.logincard}>
-    <View style={{flexDirection:'row'}}>
-    <Text style={{color:'blue',margin:20,fontWeight:'bold'}}>@</Text>
-    <Text style={{color:'blue',margin:20,fontWeight:'bold'}}>ECommerce App</Text>
-    </View>
-    
-    <Text style={{color:'grey',marginLeft:20,fontSize:10}}>
-    Use the text generator to create your own text! The Lorem Ipsum online text generator creates fictitious, fake, causal, or placeholder text.
-    </Text>
-   <View style={{margin:20}}>
-    <Text>Email</Text>
-    <TextInput style={{borderWidth:1,borderRadius:10, }}></TextInput>
-    <Text style={{marginTop:20, }}>Password</Text>
-    <TextInput style={{borderWidth:1,borderRadius:10, textAlign:''}}></TextInput>
-    <Text style={{textAlign:'right',color:'blue',fontWeight:'bold'}}>Forgot Password?</Text>
-    <TouchableOpacity>
-      <View
-        style={{
-          marginTop: 80,
-          backgroundColor: "blue",
-          height: 40,
-          color:'blue',
-          borderWidth:1,
-          borderColor:'white',
-          justifyContent: "center",
-          alignItems: "center",
-          alignContent: "center",
-          borderRadius:16
-        }}
-      >
-        
-          <Text style={{ color:'white',}}>Login</Text>
-       
+      <View style={styles.welcome}>
+        <Text
+          style={{
+            marginTop: 150,
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 30,
+          }}
+        >
+          Hello,
+        </Text>
+        <Text style={{ color: "white", fontWeight: "bold", fontSize: 30 }}>
+          Wecome back!
+        </Text>
       </View>
-      </TouchableOpacity>
-      <View style={{flexDirection:'row',justifyContent:'center'}}>
-      <Text  style={{color:'blue'}}>Don't you have account?</Text>
-      <Text style={{color:'orange', fontWeight:'bold'}}>Create one</Text>
+      <View style={styles.logincard}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={{ color: "blue", margin: 20, fontWeight: "bold" }}>
+            @
+          </Text>
+          <Text style={{ color: "blue", margin: 20, fontWeight: "bold" }}>
+            ECommerce App
+          </Text>
+        </View>
+
+        <Text style={{ color: "grey", marginLeft: 20, fontSize: 10 }}>
+          Use the text generator to create your own text! The Lorem Ipsum online
+          text generator creates fictitious, fake, causal, or placeholder text.
+        </Text>
+        <View style={{ margin: 20 }}>
+          <Text>Email</Text>
+          <TextInput style={{ borderWidth: 1, borderRadius: 10 }}></TextInput>
+          <Text style={{ marginTop: 20 }}>Password</Text>
+          <TextInput style={{ borderWidth: 1, borderRadius: 10 }}></TextInput>
+          <Text
+            style={{ textAlign: "right", color: "blue", fontWeight: "bold" }}
+          >
+            Forgot Password?
+          </Text>
+          <TouchableOpacity>
+            <View
+              style={{
+                marginTop: 80,
+                backgroundColor: "blue",
+                height: 40,
+                color: "blue",
+                borderWidth: 1,
+                borderColor: "white",
+                justifyContent: "center",
+                alignItems: "center",
+                alignContent: "center",
+                borderRadius: 16,
+              }}
+            >
+              <Text style={{ color: "white" }}>Login</Text>
+            </View>
+          </TouchableOpacity>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <Text style={{ color: "blue" }}>Don't you have account?</Text>
+            <Text style={{ color: "orange", fontWeight: "bold" }}>
+              Create one
+            </Text>
+          </View>
+        </View>
       </View>
-      
-   </View>
-
     </View>
-
-    </View>
-  )
-    
+  );
 }
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'blue',
-    },
-    welcome:{
-        flex:1,
-        backgroundColor:'',
-        marginTop:30
-    },
-    logincard:{
-        flex:2,
-        backgroundColor:'white',
-        borderTopEndRadius:20,
-        borderTopLeftRadius:16,
-
-    },
-    
-})
-
-
-
-
-
-
-
-
-
-
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+  },
+  welcome: {
+    flex: 1,
+    backgroundColor: "",
+    marginTop: 30,
+  },
+  logincard: {
+    flex: 2,
+    backgroundColor: "white",
+    borderTopEndRadius: 20,
+    borderTopLeftRadius: 16,
+  },
+});
